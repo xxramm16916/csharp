a6a71edc553767ea20e2ab788bc70f61271c2e1d
Hramov <hramov.andrey@inbox.ru>
2020-01-04 13:15:22 +0300

    Тестовое задание

---
+++ ImageDownloadProject/ImageDownloadProject/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ImageDownloadProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly string fileType = "image/jpg";
+
+        public IActionResult Index()
+        {          
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> DownloadImage(string url)
+        {
+            byte[] file = null;
+            using (var client = new HttpClient())
+            {
+
+                using (var result = await client.GetAsync(url))
+                {
+                    if (result.IsSuccessStatusCode)
+                    {
+                        file = await result.Content.ReadAsByteArrayAsync();
+                    }
+
+                }
+            }
+            return File(file, fileType);
+        }
+    }
+}
